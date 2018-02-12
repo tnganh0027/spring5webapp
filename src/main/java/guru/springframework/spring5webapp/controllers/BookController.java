bb0a4e7fa6c4ce047a146035edd54a88df969ec7
tnganh0027 <tnganh0027@gmail.com>
2018-02-12 14:23:04 +0700

    simple MVC with view the list of books and authors

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    //return the view name.
+    @RequestMapping("/books")
+    public String getBook(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+
+    }
+}
